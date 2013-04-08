69142aec060f0a453baec2aeddc1c483d62a6ef8
rdodev <rubenoz+github@gmail.com>
2013-04-07 20:44:43 -0400

    More S3 Progress

--- app/scripts/controllers/ResourcesS3.js
+++ app/scripts/controllers/ResourcesS3.js
@@ -1,10 +1,27 @@
 'use strict';
 
 angular.module('cfApp')
-  .controller('ResourcesS3Ctrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ResourcesS3Ctrl', function ($scope, $rootScope, $location) {
+    $scope.S3Resouce = {"Type" : "AWS::S3::Bucket"};
+    $scope.acList = ["Private", "PublicRead", "PublicReadWrite", "AuthenticatedRead", "BucketOwnerRead", "BucketOwnerFullControl"];
+    $scope.bucketName = "";
+    $scope.accessControl = "";
+    $scope.indexDocument = "";
+    $scope.errorDocument = "";
+
+    $scope.addS3Resource = function () {
+        var props = {};
+        var i = parseInt($scope.accessControl, 10) || 0;
+        props.AccessControl =  $scope.acList[i];
+        if($scope.indexDocument || $scope.errorDocument) {
+            props.WebsiteConfiguration = {};
+            props.WebsiteConfiguration.IndexDocument = $scope.indexDocument;
+            props.WebsiteConfiguration.errorDocument = $scope.errorDocument;
+        }
+    };
+
+    $scope.cancel = function () {
+        $location.path('/resources');
+    };
+
   });
