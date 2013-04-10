5ce944a00aefee37fa5d329e2c894c9888e9b39e
rdodev <rubenoz+github@gmail.com>
2013-04-09 20:15:12 -0400

    Begin EC2 Work

--- app/scripts/controllers/ResourcesEC2.js
+++ app/scripts/controllers/ResourcesEC2.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('cfApp')
-  .controller('ResourcesEC2Ctrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ResourcesEC2Ctrl', function ($scope, $rootScope, $location) {
+    $scope.EC2Resource= {"Type" : "AWS::EC2::Instance"};
+    $scope.typeList = ["t1.micro", "m1.small", "m1.medium", "m1.large", "m1.xlarge", "c1.xlarge", "c1.medium", "m2.xlarge"];
+    $scope.zones = ["us-east-1a","us-east-1b", "us-east-1c","us-east-1d","us-west-2a","us-west-2b","us-west-2c", "us-west-1a", "us-west-1c"];
+    $scope.disableTerminationValues = [{"yes":"true"},{"no", "false"}];
+    $scope.az = "";
+    $scope.instanceType = "";
+    $scope.imageId = "";
+
+
   });
