a2a1d859a1712d44fa810dee030d35fb7b233a4d
Ruben Orduz <rubenoz@gmail.com>
2013-08-20 11:29:01 -0700

    more tests. Woo

--- test/spec/controllers/Output.js
+++ test/spec/controllers/Output.js
@@ -6,19 +6,32 @@ describe('Controller: OutputCtrl', function () {
   beforeEach(module('cfApp'));
 
   var OutputCtrl,
-    scope,rootScope;
+    scope,rootScope, loc;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller) {
+  beforeEach(inject(function ($controller, $rootScope, $location) {
     scope = {};
     rootScope = {};
+    loc = $location;
     OutputCtrl = $controller('OutputCtrl', {
       $scope: scope,
-      $rootScope: rootScope
+      $rootScope: rootScope,
+      $location: loc
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
+  it('checking json function works as intended', function () {
     expect(scope.output).toBeUndefined();
+    rootScope.cfObj = {};
+    scope.json();
+    expect(scope.output).not.toBeUndefined();
   });
+
+  it('checking reset function works as intended', function () {
+    spyOn(loc, 'path');
+    scope.reset()
+    expect(rootScope.cfObj).toEqual({});
+    expect(loc.path).toHaveBeenCalledWith('/');
+  });
+
 });
