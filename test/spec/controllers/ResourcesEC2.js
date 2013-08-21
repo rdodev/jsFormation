54fc6d4c69a9d8450b33bb620f42c0c5aa792ef4
rdodev <rubenoz+github@gmail.com>
2013-08-21 19:21:00 -0400

    More tests! woot!

--- test/spec/controllers/ResourcesEC2.js
+++ test/spec/controllers/ResourcesEC2.js
@@ -6,17 +6,59 @@ describe('Controller: ResourcesEC2Ctrl', function () {
   beforeEach(module('cfApp'));
 
   var ResourcesEC2Ctrl,
-    scope;
+    scope, rootScope, loc;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller) {
+  beforeEach(inject(function ($controller, $location) {
     scope = {};
+    rootScope = {};
+    loc = $location;
+
+    scope.instanceName = 'TestInstance';
+    scope.AvailabilityZone = 'us-east-1';
+    scope.InstanceType = 'm1.medium';
+    scope.ImageId = '';
+    scope.DisableApiTermination = '';
+    scope.KeyName = '';
+    rootScope.cfObj = {};
+    rootScope.cfObj.Resources = {};
+    rootScope.ec2 = 0;
+
     ResourcesEC2Ctrl = $controller('ResourcesEC2Ctrl', {
-      $scope: scope
+      $scope: scope,
+      $rootScope: rootScope,
+      $location: loc
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.addEC2Resource).not.toBeNull();
+  it('addEC2Resource should set p.DisableApiTermination to true if not chosen', function () {
+    scope.addEC2Resource();
+    expect(scope.EC2Resource.Properties).not.toBe({});
+    expect(scope.EC2Resource.Properties.DisableApiTermination).toEqual('true');
   });
+  
+  it('addEC2Resource should set KeyName if present', function () {
+    scope.KeyName = 'bippiddi';
+    scope.addEC2Resource();
+    expect(scope.EC2Resource.Properties).not.toBe({});
+    expect(scope.EC2Resource.Properties.KeyName).toEqual('bippiddi');
+  });
+
+  it('addEC2Resource should increase rootScope.ec2 by 1', function () {
+    scope.addEC2Resource();
+    expect(rootScope.ec2).toEqual(1);
+  });
+
+  it('addEC2Resource should redirect to /resources when done', function () {
+    spyOn(loc, 'path');
+    scope.addEC2Resource();
+    expect(loc.path).toHaveBeenCalledWith('/resources');
+  });
+
+  it('cancel should redirect to /resources', function () {
+    spyOn(loc, 'path');
+    scope.cancel();
+    expect(loc.path).toHaveBeenCalledWith('/resources');
+  });
+
 });
