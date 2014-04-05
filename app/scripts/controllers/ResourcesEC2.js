4a7e21dbeeab78838eb661595020cfdfe691c684
rdodev <rubenoz@gmail.com>
2014-04-05 17:59:53 -0400

    Bug fixes and coverage

--- app/scripts/controllers/ResourcesEC2.js
+++ app/scripts/controllers/ResourcesEC2.js
@@ -7,19 +7,15 @@ angular.module('cfApp')
     $scope.typeList = ['t1.micro', 'm1.small', 'm1.medium', 'm1.large', 'm1.xlarge', 'c1.xlarge', 'c1.medium', 'm2.xlarge'];
     $scope.zones = ['us-east-1a','us-east-1b', 'us-east-1c','us-east-1d','us-west-2a','us-west-2b','us-west-2c', 'us-west-1a', 'us-west-1c'];
     $scope.disableTerminationValues = [{'key': 'yes', 'value':'true'}, {'key':'no', 'value':'false'}];
-    $scope.instanceName = '';
-    $scope.AvailabilityZone = '';
-    $scope.InstanceType = '';
-    $scope.ImageId = '';
-    $scope.DisableApiTermination = '';
-    $scope.KeyName = '';
 
     $scope.addEC2Resource = function () {
         var p = {};
         p.AvailabilityZone = $scope.AvailabilityZone;
         p.InstanceType = $scope.InstanceType;
-        p.DisableApiTermination = $scope.DisableApiTermination || 'true'; //erring on the safe side
-        p.ImageId = $scope.ImageId;
+        p.DisableApiTermination = $scope.DisableApiTermination ? 
+                                    !!$scope.DisableApiTermination['value']:
+                                    true;
+        p.ImageId = $scope.ImageID;
         if ($scope.KeyName) {
             p.KeyName = $scope.KeyName;
         }
--- test/spec/controllers/ResourcesEC2.js
+++ test/spec/controllers/ResourcesEC2.js
@@ -6,56 +6,67 @@ describe('Controller: ResourcesEC2Ctrl', function () {
   beforeEach(module('cfApp'));
 
   var ResourcesEC2Ctrl,
-    scope, rootScope, loc;
+    scope, rootScope, loc, ctrl;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $location) {
     scope = {};
     rootScope = {};
     loc = $location;
-
+    ctrl = $controller;
     scope.instanceName = 'TestInstance';
     scope.AvailabilityZone = 'us-east-1';
     scope.InstanceType = 'm1.medium';
-    scope.ImageId = '';
-    scope.DisableApiTermination = '';
-    scope.KeyName = '';
     rootScope.cfObj = {};
     rootScope.cfObj.Resources = {};
     rootScope.ec2 = 0;
-
-    ResourcesEC2Ctrl = $controller('ResourcesEC2Ctrl', {
-      $scope: scope,
-      $rootScope: rootScope,
-      $location: loc
-    });
+    this.init = function () {
+      ResourcesEC2Ctrl = $controller('ResourcesEC2Ctrl', {
+        $scope: scope,
+        $rootScope: rootScope,
+        $location: loc
+      });
+    };
   }));
 
   it('addEC2Resource should set p.DisableApiTermination to true if not chosen', function () {
+    this.init();
     scope.addEC2Resource();
     expect(scope.EC2Resource.Properties).not.toBe({});
-    expect(scope.EC2Resource.Properties.DisableApiTermination).toEqual('true');
+    expect(scope.EC2Resource.Properties.DisableApiTermination).toEqual(true);
+  });
+
+  it('addEC2Resource should set p.DisableApiTermination as false', function () {
+    scope.DisableApiTermination = {key: 'yes', value: 'true'};
+    this.init();
+    scope.addEC2Resource();
+    expect(scope.EC2Resource.Properties).not.toBe({});
+    expect(scope.EC2Resource.Properties.DisableApiTermination).toEqual(true);
   });
   
   it('addEC2Resource should set KeyName if present', function () {
     scope.KeyName = 'bippiddi';
+    this.init();
     scope.addEC2Resource();
     expect(scope.EC2Resource.Properties).not.toBe({});
     expect(scope.EC2Resource.Properties.KeyName).toEqual('bippiddi');
   });
 
   it('addEC2Resource should increase rootScope.ec2 by 1', function () {
+    this.init();
     scope.addEC2Resource();
     expect(rootScope.ec2).toEqual(1);
   });
 
   it('addEC2Resource should redirect to /resources when done', function () {
+    this.init();
     spyOn(loc, 'path');
     scope.addEC2Resource();
     expect(loc.path).toHaveBeenCalledWith('/resources');
   });
 
   it('cancel should redirect to /resources', function () {
+    this.init();
     spyOn(loc, 'path');
     scope.cancel();
     expect(loc.path).toHaveBeenCalledWith('/resources');
